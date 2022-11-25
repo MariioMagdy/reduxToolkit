b85a9609389bce7706f5ce76f274548949d4d069
MariioMagdy <mariiomagdy@gmail.com>
2022-11-25 19:16:42 +0200

    add fun

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -3,7 +3,9 @@ import { Button, ButtonGroup } from "react-bootstrap";
 
 export const PostListItem = ({ data, loading, error, deleteRecord }) => {
   const deleteHandler = (item) => {
-    if (window.confirm("Are your sure that you want to Delete this item ?")) {
+    if (
+      window.confirm(`Are your sure that you want to Delete ${item.title} ?`)
+    ) {
       deleteRecord(item.id);
     }
   };
@@ -22,7 +24,7 @@ export const PostListItem = ({ data, loading, error, deleteRecord }) => {
           return (
             <tr key={element.id}>
               <td>{++idx}</td>
-              <td>{element.description}</td>
+              <td>{element.title}</td>
               <td>
                 <ButtonGroup aria-label="Basic example">
                   <Button variant="success">Edit</Button>
--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,5 +1,47 @@
 import React from "react";
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { insertPost } from "../state/postSlice";
+import { useDispatch } from "react-redux";
 
 export const Add = () => {
-  return <div>Add</div>;
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const dispatch = useDispatch();
+  const formHandler = (e) => {
+    e.preventDefault();
+    const id = Date.now();
+    console.log(id);
+    dispatch(insertPost({ id, title, description }));
+  };
+
+  return (
+    <div>
+      <Form onSubmit={formHandler}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="name@example.com"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 };
